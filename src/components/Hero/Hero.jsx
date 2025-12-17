321f0c5b7c19eda2ac973377c8844c975ed02da8
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-12-17 13:18:26 +0200

    hero commponent -home- is initially finished

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='text-white bg-blue-600'>hero</div>
+    <header className="bg-gray-2 h-screen overflow-hidden flex justify-center items-center">
+      <div className="text-center w-3/4 ">
+        <img src="/src/assets/logo.png" className="w-52 mx-auto" alt="Adham Gamal Logo" />
+        <div className="container text-zinc-200">
+          <h1 className="font-main text-3xl position-relative my-3 text-center z-10  font-bold">Launch <span className="text-blue-600">R</span>eact</h1>
+          <p className="font-regular position-relative">LaunchR is a lightweight React 19 project designed to showcase core React fundamentals in a clean, beginner-friendly way.The project focuses on demonstrating essential concepts such as component-based architecture, routing, basic state handling, and project structure without unnecessary complexity or overengineering.</p> 
+        </div> 
+      </div>     
+    </header>
+
   )
 }
