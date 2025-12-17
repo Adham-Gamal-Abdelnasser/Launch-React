603573c48eaa3707d41517c2b604c675f6c84cb8
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-12-17 13:09:33 +0200

    navbar component initially finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Hero from './components/hero/hero.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 function App() {
 
   return (
     <>
-    <p>hello</p>
+      <Navbar></Navbar>
     </>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+      <nav className='relative top-0 start-0 end-0 bg-gray-1 flex justify-around items-center py-4 text-zinc-200 font-main'>
+        <h2 className='text-3xl'>Launch<span className='text-blue-600'>R</span></h2>
+        <ul className='flex gap-5'>
+          <li><a className='' href="http://">Home</a></li>
+          <li><a className='' href="http://">About</a></li>
+          <li><a className='' href="http://">Contact</a></li>
+        </ul>
+      </nav>
+    </>
   )
 }
