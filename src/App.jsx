8842d872f5361db30009fe11bb3b3e38c2c398fa
Adham-Gamal-Abdelnasser <aboeladahem694@gmail.com>
2025-12-17 12:06:34 +0200

    project initialization

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Hero from './components/hero/hero.jsx'
+
+function App() {
+
+  return (
+    <>
+    <p>hello</p>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='text-white bg-blue-600'>hero</div>
+  )
+}
